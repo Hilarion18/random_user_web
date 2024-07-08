77bcf291961f49dea73100830655f7eee7c9421f
macbook <macbook@macbooks-MacBook-Pro.local>
2024-07-08 12:03:14 +0700

    add folder out

--- app/(pages)/user/[id]/page.tsx
+++ app/(pages)/user/[id]/page.tsx
@@ -27,6 +27,7 @@ export function generateStaticParams() {
   return [{ id: '1' }, { id: '2' }, { id: '3' }]
 }
 
+
 export default function Page({ params }: { params: { id: string } }) {
   const { id } = params
   return (
