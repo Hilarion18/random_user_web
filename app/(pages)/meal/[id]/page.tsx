041d122e7395e2db63bd4fc151de894bfccfa180
macbook <macbook@macbooks-MacBook-Pro.local>
2024-07-08 10:20:21 +0700

    update generateStaticParams

--- app/(pages)/meal/[id]/page.tsx
+++ app/(pages)/meal/[id]/page.tsx
@@ -22,8 +22,13 @@ export const metadata: Metadata = {
   description:
     'Pengepul data driver'
 };
+export async function generateStaticParams() {
+  // const posts = await fetch('https://.../posts').then((res) => res.json())
+ 
+  return ""
+}
 
-export default function generateStaticParams() {
+export default function Page() {
   return (
     <>
       <MealDetail/>
